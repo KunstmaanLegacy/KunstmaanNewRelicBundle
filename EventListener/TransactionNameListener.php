9395acf003be42e1de20767b61d7af26d1ec4b28
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2012-11-12 17:56:01 +0100

    only log master requests

--- EventListener/TransactionNameListener.php
+++ EventListener/TransactionNameListener.php
@@ -3,17 +3,18 @@
 namespace Kunstmaan\NewRelicBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class TransactionNameListener
 {
 
     public function setTransactionName(FilterControllerEvent $event)
     {
-        if(function_exists("newrelic_name_transaction")){
+        if (function_exists("newrelic_name_transaction") &&
+                $event->getRequestType() == HttpKernelInterface::MASTER_REQUEST) {
             $controller = $event->getRequest()->attributes->get('_controller');
             $arr = array_reverse(explode('\\', $controller));
-            newrelic_name_transaction($arr[0]);
+           newrelic_name_transaction($arr[0]);
         }
     }
-
 }
